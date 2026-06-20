def57a5a380419d422ff094255ed6dd7b5aab802
Madhur Prakash <madhurprakash2005@gmail.com>
2026-06-21 02:13:45 +0530

    added multiple wallets

--- frontend/src/lib/walletkit.ts
+++ frontend/src/lib/walletkit.ts
@@ -1,16 +1,29 @@
-// StellarWalletsKit v2 uses a static-class pattern (no instantiation)
 export { StellarWalletsKit, Networks } from "@creit.tech/stellar-wallets-kit";
 export { FreighterModule, FREIGHTER_ID } from "@creit.tech/stellar-wallets-kit/modules/freighter";
+export { AlbedoModule, ALBEDO_ID } from "@creit.tech/stellar-wallets-kit/modules/albedo";
+export { xBullModule, XBULL_ID } from "@creit.tech/stellar-wallets-kit/modules/xbull";
+export { LobstrModule, LOBSTR_ID } from "@creit.tech/stellar-wallets-kit/modules/lobstr";
+export { RabetModule, RABET_ID } from "@creit.tech/stellar-wallets-kit/modules/rabet";
 
 import { StellarWalletsKit, Networks } from "@creit.tech/stellar-wallets-kit";
 import { FreighterModule, FREIGHTER_ID } from "@creit.tech/stellar-wallets-kit/modules/freighter";
+import { AlbedoModule } from "@creit.tech/stellar-wallets-kit/modules/albedo";
+import { xBullModule } from "@creit.tech/stellar-wallets-kit/modules/xbull";
+import { LobstrModule } from "@creit.tech/stellar-wallets-kit/modules/lobstr";
+import { RabetModule } from "@creit.tech/stellar-wallets-kit/modules/rabet";
 
 let initialized = false;
 
 export function initWalletKit() {
   if (initialized) return;
   StellarWalletsKit.init({
-    modules: [new FreighterModule()],
+    modules: [
+      new FreighterModule(),
+      new AlbedoModule(),
+      new xBullModule(),
+      new LobstrModule(),
+      new RabetModule(),
+    ],
     network: Networks.TESTNET,
     selectedWalletId: FREIGHTER_ID,
   });
