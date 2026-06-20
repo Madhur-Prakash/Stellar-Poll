721c49e9d987c74bcdf49c21584fb4fb54ea500c
Madhur Prakash <madhurprakash2005@gmail.com>
2026-06-21 02:15:03 +0530

    added multiple wallets(11 supported)

--- frontend/src/lib/walletkit.ts
+++ frontend/src/lib/walletkit.ts
@@ -4,6 +4,12 @@ export { AlbedoModule, ALBEDO_ID } from "@creit.tech/stellar-wallets-kit/modules
 export { xBullModule, XBULL_ID } from "@creit.tech/stellar-wallets-kit/modules/xbull";
 export { LobstrModule, LOBSTR_ID } from "@creit.tech/stellar-wallets-kit/modules/lobstr";
 export { RabetModule, RABET_ID } from "@creit.tech/stellar-wallets-kit/modules/rabet";
+export { HanaModule, HANA_ID } from "@creit.tech/stellar-wallets-kit/modules/hana";
+export { BitgetModule, BITGET_WALLET_ID } from "@creit.tech/stellar-wallets-kit/modules/bitget";
+export { KleverModule, KLEVER_ID } from "@creit.tech/stellar-wallets-kit/modules/klever";
+export { OneKeyModule, ONEKEY_ID } from "@creit.tech/stellar-wallets-kit/modules/onekey";
+export { CactusLinkModule, CACTUSLINK_ID } from "@creit.tech/stellar-wallets-kit/modules/cactuslink";
+export { HotWalletModule, HOTWALLET_ID } from "@creit.tech/stellar-wallets-kit/modules/hotwallet";
 
 import { StellarWalletsKit, Networks } from "@creit.tech/stellar-wallets-kit";
 import { FreighterModule, FREIGHTER_ID } from "@creit.tech/stellar-wallets-kit/modules/freighter";
@@ -11,6 +17,12 @@ import { AlbedoModule } from "@creit.tech/stellar-wallets-kit/modules/albedo";
 import { xBullModule } from "@creit.tech/stellar-wallets-kit/modules/xbull";
 import { LobstrModule } from "@creit.tech/stellar-wallets-kit/modules/lobstr";
 import { RabetModule } from "@creit.tech/stellar-wallets-kit/modules/rabet";
+import { HanaModule } from "@creit.tech/stellar-wallets-kit/modules/hana";
+import { BitgetModule } from "@creit.tech/stellar-wallets-kit/modules/bitget";
+import { KleverModule } from "@creit.tech/stellar-wallets-kit/modules/klever";
+import { OneKeyModule } from "@creit.tech/stellar-wallets-kit/modules/onekey";
+import { CactusLinkModule } from "@creit.tech/stellar-wallets-kit/modules/cactuslink";
+import { HotWalletModule } from "@creit.tech/stellar-wallets-kit/modules/hotwallet";
 
 let initialized = false;
 
@@ -23,6 +35,12 @@ export function initWalletKit() {
       new xBullModule(),
       new LobstrModule(),
       new RabetModule(),
+      new HanaModule(),
+      new BitgetModule(),
+      new KleverModule(),
+      new OneKeyModule(),
+      new CactusLinkModule(),
+      new HotWalletModule(),
     ],
     network: Networks.TESTNET,
     selectedWalletId: FREIGHTER_ID,
